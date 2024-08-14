e913cbb467f0d9483552116264c872a3b58dfef4
Icaro Jordano <Icarojordano>
2024-08-14 00:29:07 -0300

    tentando primeiro deploy

--- src/Elements/Titulo.jsx
+++ src/Elements/Titulo.jsx
@@ -1,9 +1,10 @@
+import "./../Elements/Titulo.css"
+
 function Titulo({text}){
     return (
         <>
-        <p className="titulo" > ---------------------------------------------------------------
-            <h1>{text}</h1>
-            ---------------------------------------------------------------
+        <p className="titulo" ><h1>{text}</h1>
+        
         </p></>
     )
 }
